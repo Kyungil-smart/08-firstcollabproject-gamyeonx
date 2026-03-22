626534c8d156adbc9b0eb9e76af85b6888ce6aae
Geunhyung Song <gamyeonx@gmail.com>
2026-03-22 12:30:03 +0900

    [Add] 인벤토리 기능 추가
    추후 건물의 생성 버튼 시스템 및 상점 시스템을 위한 연습 및 밑작업

---
+++ Assets/Members/SGH/Scripts/InventoryUI.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class InventoryUI : MonoBehaviour
+{
+    public GameObject InventoryPanel;
+    bool activeInventory = false;
+
+    private void Start()
+    {
+        InventoryPanel.SetActive(activeInventory);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I))
+        {
+            activeInventory = !activeInventory;
+            InventoryPanel.SetActive(activeInventory);
+        }
+    }
+}
