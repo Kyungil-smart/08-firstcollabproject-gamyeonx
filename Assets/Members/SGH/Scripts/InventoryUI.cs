f9d2f4781f6adca40a074b8fafc254420cd50906
Geunhyung Song <gamyeonx@gmail.com>
2026-03-22 13:02:51 +0900

    [Add] 인벤토리 슬롯 확장 기능 추가
    추후 길드원 슬롯 추가버튼 등에 필요할수도 있다고 생각합니다.

--- Assets/Members/SGH/Scripts/InventoryUI.cs
+++ Assets/Members/SGH/Scripts/InventoryUI.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryUI : MonoBehaviour
 {
+    private Inventory _inventory;
+
     public GameObject InventoryPanel;
     bool activeInventory = false;
 
+    public Slot[] slots;
+    public Transform slotHolder;
+
     private void Start()
     {
+        _inventory = Inventory.instance;
+        slots = slotHolder.GetComponentsInChildren<Slot>();
+        _inventory.onSlotCountChange += SlotChange;
         InventoryPanel.SetActive(activeInventory);
+    }
+
+     
+    private void SlotChange(int val)
+    {
+        for (int i = 0; i < slots.Length; i++)
+        {
+            if (i < _inventory.slotCnt)
+                slots[i].GetComponent<Button>().interactable = true;
+            else slots[i].GetComponent<Button>().interactable = false;
+        }
     }
 
     private void Update()
@@ -17,5 +37,10 @@ public class InventoryUI : MonoBehaviour
             activeInventory = !activeInventory;
             InventoryPanel.SetActive(activeInventory);
         }
+    }
+
+    public void AddSlot()
+    {
+        _inventory.slotCnt++;
     }
 }
