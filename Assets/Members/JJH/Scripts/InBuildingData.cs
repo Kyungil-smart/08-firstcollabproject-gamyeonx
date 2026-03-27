4cc5642f0f7c4c245651b19760c7e9c253d3f762
Jaehoon Jeong <wogns4168@naver.com>
2026-03-27 18:10:24 +0900

    [Add] 건물 내부 Tilemap 설치 로직 추가

--- Assets/Members/JJH/Scripts/InBuildingData.cs
+++ Assets/Members/JJH/Scripts/InBuildingData.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
+using UnityEngine.Tilemaps;
 using UnityEngine.UI;
 
 public class InBuildingData : MonoBehaviour
@@ -25,17 +26,23 @@ public class InBuildingData : MonoBehaviour
     public GameObject WaitPivot;
     public GameObject UsePivot;
     
+    [Header("내부 그리드 설정")]
+    public List<GameObject> _whiteAreaPivots;  // 흰 타일 생성 위치 목록
+    private Vector2Int _whiteAreaSize;  // 각 피벗에서 생성할 크기
+    
     CameraController _cameraController;
 
     private void Awake()
     {
         _canvas.gameObject.SetActive(false);
+        _whiteAreaSize =  new Vector2Int(1, 1);
     }
 
     private void Start()
     {
         _returnButton.onClick.AddListener(ReturnButton);
         _cameraController = FindFirstObjectByType<CameraController>();
+        InBuildingWhiteTilesCreate();  // 내부 진입 시 흰 타일 자동 생성
     }
 
     public void BuildingEntered()
@@ -49,7 +56,14 @@ public class InBuildingData : MonoBehaviour
         float X = CameraPivot.transform.position.x;
         float Y = CameraPivot.transform.position.y;
         Vector2 NextLevelPivot = new Vector2(X, Y);
-        Instantiate(_nextLevelPrefabs[currentLevel - 1], NextLevelPivot, CameraPivot.transform.rotation);
+        GameObject nextLevelInstance = Instantiate(_nextLevelPrefabs[currentLevel - 1], NextLevelPivot, CameraPivot.transform.rotation);
+        LevelUpBuildingData levelUpData = nextLevelInstance.GetComponent<LevelUpBuildingData>();
+        if (levelUpData != null)
+        {
+            // 3. 한 프레임 쉬고 실행하거나, 강제로 좌표 동기화
+            Physics2D.SyncTransforms(); 
+            levelUpData.LevelUpBuildingWhiteTilesCreate();
+        }
         currentLevel++;
     }
 
@@ -57,5 +71,26 @@ public class InBuildingData : MonoBehaviour
     {
         _cameraController.ReturnToWorld();
         _canvas.gameObject.SetActive(false);
+    }
+    
+    private void InBuildingWhiteTilesCreate()
+    {
+        if (GridBuildingSystem.Instance.MainTilemap == null) return;
+
+        TileBase whiteTile = Resources.Load<TileBase>("SGH_Test/white");
+
+        foreach (var pivot in _whiteAreaPivots)
+        {
+            Vector3Int cellPos = GridBuildingSystem.Instance.MainTilemap.WorldToCell(pivot.transform.position);
+            
+            for (int x = 0; x < _whiteAreaSize.x; x++)
+            {
+                for (int y = 0; y < _whiteAreaSize.y; y++)
+                {
+                    Vector3Int pos = new Vector3Int(cellPos.x + x, cellPos.y + y, 0);
+                    GridBuildingSystem.Instance.MainTilemap.SetTile(pos, whiteTile);
+                }
+            }
+        }
     }
 }
