50a790bb09ff053ceb81bdc6505624b44cb36cf4
Geunhyung Song <gamyeonx@gmail.com>
2026-03-29 10:03:11 +0900

    [Fix] 건물 회전 버그 수정
    건물 회전시 건물의 타일 점유영역과 실제 이미지(입구, 출구포함)가 점유영역에 맞지않게 회전하는 부분 수정.

--- Assets/Scripts/Maps/Building.cs
+++ Assets/Scripts/Maps/Building.cs
@@ -7,6 +7,12 @@ public enum BuildType
     Building
 }
 
+public enum ETurnType
+{
+    SquareTurn,
+    TwoxFourTurn,
+}
+
 public class Building : MonoBehaviour
 {
     public bool Placed { get; private set; }    // 설치 완료 여부
@@ -21,7 +27,11 @@ public class Building : MonoBehaviour
     public InBuildingData InBuildingData;
     public GameObject InBuildingRoot;
 
-    
+    // 회전 전용 필드
+    public ETurnType turnType; // 회전 방식
+    public Transform pivot; // 회전용 피벗
+    public int rotateCount = 0; // 회전 횟수 카운트
+
     private void Awake()
     {
         _canvas = GetComponentInChildren<Canvas>(true);
@@ -91,14 +101,65 @@ public class Building : MonoBehaviour
         GridBuildingSystem.Instance.TakeArea(areaTemp);
     }
 
+    //--- 회전 로직 ---
+
     // 오브젝트 회전시키는 메서드
     public void Rotate()
     {
-        transform.Rotate(0, 0, -90);
+        if (Placed) return;
+
+        switch (turnType)
+        {
+            case ETurnType.SquareTurn: RotateSquareBuilding(); break;
+            case ETurnType.TwoxFourTurn: RotateTwoxFourBuilding(); break;
+                // 필요시 다른 타입 추가 및 함수 추가 제작 필요할수도 있음
+        }
+    }
+
+    // 정사각형용 회전
+    private void RotateSquareBuilding()
+    {
+        pivot.Rotate(0, 0, 90);
+        rotateCount = (rotateCount + 1) % 4;
 
         var size = area.size;
         area.size = new Vector3Int(size.y, size.x, size.z);
     }
+
+    // 여기서 부터는 추후 만들어질 건물의 크기에 맞게 함수를 따로 만들어야함.
+
+    private void RotateTwoxFourBuilding()
+    {
+        if (Placed) return;
+
+        pivot.Rotate(0, 0, 90);
+
+        var size = area.size;
+        area.size = new Vector3Int(size.y, size.x, size.z);
+
+        rotateCount = (rotateCount + 1) % 4;
+
+        Vector3 offset = Vector3.zero;
+        switch (rotateCount)
+        {
+            case 1:
+                offset = new Vector3(0, -1, 0);
+                break;
+            case 2:
+                offset = new Vector3(-2, -1, 0);
+                break;
+            case 3:
+                offset = new Vector3(-2, -3, 0);
+                break;
+            case 0:
+                offset = new Vector3(0, -3, 0);
+                break;
+        }
+
+        pivot.Translate(offset, Space.Self);
+    }
+
+    //------------
 
     // 오브젝트 재배치용 메서드
     public void StartMove()
