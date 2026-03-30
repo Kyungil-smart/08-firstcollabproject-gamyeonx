3cb6a40a36c01654cef1d135d14e4db098ba94a9
연동준 (플밍 4기) <skg023863@gmail.com>
2026-03-30 09:48:59 +0900

    [Add] 실내 프리팹 제작 / 업그레이드 구현

--- Assets/Members/JJH/Scripts/InBuildingData.cs
+++ Assets/Members/JJH/Scripts/InBuildingData.cs
@@ -19,15 +19,25 @@ public class InBuildingData : MonoBehaviour
     
     [Header("다음레벨 프리팹")]
     [SerializeField] private List<GameObject> _nextLevelPrefabs;
+    [SerializeField] private GameObject _UpgradeExpandArea; 
+    
+    private FacilityRuntime _facilityRuntime;
     
     [Header("입구/웨이팅/사용 정보")]
     public GameObject EnterPivot;
-
     public GameObject WaitPivot;
     public GameObject UsePivot;
+    public List<GameObject> UsePivots;
+
+    public GameObject EntrancePivot;
+    public GameObject ExitPivot;
+    public List<GameObject> EntranceWayPivots;
+    public List<GameObject> ExitWayPivots;
+    
     
     [Header("내부 그리드 설정")]
     public List<GameObject> _whiteAreaPivots;  // 흰 타일 생성 위치 목록
+    public List<GameObject> _upgradeWhiteAreaPivots; // 업그레이드 후 휜 타일 생성 위치 목록
     private Vector2Int _whiteAreaSize;  // 각 피벗에서 생성할 크기
     
     CameraController _cameraController;
@@ -36,6 +46,7 @@ public class InBuildingData : MonoBehaviour
     {
         _canvas.gameObject.SetActive(false);
         _whiteAreaSize =  new Vector2Int(1, 1);
+        _UpgradeExpandArea.SetActive(false);
     }
 
     private void Start()
@@ -44,26 +55,44 @@ public class InBuildingData : MonoBehaviour
         _cameraController = FindFirstObjectByType<CameraController>();
         InBuildingWhiteTilesCreate();  // 내부 진입 시 흰 타일 자동 생성
     }
+    
 
     public void BuildingEntered()
     {
         _canvas.gameObject.SetActive(true);
     }
 
+    public void SetFacilityRuntime(FacilityRuntime facilityRuntime)
+    {
+        _facilityRuntime = facilityRuntime;
+    }
+
     public void BuildingLevelUp()
     {
-        if (maxLevel <= currentLevel) return;
-        float X = CameraPivot.transform.position.x;
-        float Y = CameraPivot.transform.position.y;
-        Vector2 NextLevelPivot = new Vector2(X, Y);
-        GameObject nextLevelInstance = Instantiate(_nextLevelPrefabs[currentLevel - 1], NextLevelPivot, CameraPivot.transform.rotation);
-        LevelUpBuildingData levelUpData = nextLevelInstance.GetComponent<LevelUpBuildingData>();
-        if (levelUpData != null)
+        if (maxLevel < currentLevel) return;
+        // float X = CameraPivot.transform.position.x;
+        // float Y = CameraPivot.transform.position.y;
+        // Vector2 NextLevelPivot = new Vector2(X, Y);
+        // GameObject nextLevelInstance = Instantiate(_nextLevelPrefabs[currentLevel - 1], NextLevelPivot, CameraPivot.transform.rotation);
+        // LevelUpBuildingData levelUpData = nextLevelInstance.GetComponent<LevelUpBuildingData>();
+        // if (levelUpData != null)
+        // {
+        //     // 3. 한 프레임 쉬고 실행하거나, 강제로 좌표 동기화
+        //     Physics2D.SyncTransforms(); 
+        //     levelUpData.LevelUpBuildingWhiteTilesCreate();
+        // }
+        if (currentLevel == 1)
         {
-            // 3. 한 프레임 쉬고 실행하거나, 강제로 좌표 동기화
-            Physics2D.SyncTransforms(); 
-            levelUpData.LevelUpBuildingWhiteTilesCreate();
+            _UpgradeExpandArea.SetActive(true); // 업그레이드 프리팹 SetActive(true)
+            for (int i = 0; i < _upgradeWhiteAreaPivots.Count; i++)
+            {
+                _whiteAreaPivots.Add(_upgradeWhiteAreaPivots[i]); // 업그레이드 프리팹에 있는 휜 타일 생성 위치 갱신
+            }
+
+            InBuildingWhiteTilesCreate(); // 업그레이드 프리팹에 있는 휜 타일 생성
         }
+        else if (currentLevel == 2) _facilityRuntime.UpgradePrice(20);
+        Debug.Log($"<color=yellow/>현재 가격 {_facilityRuntime.Gold}</color>");
         currentLevel++;
     }
 
@@ -88,6 +117,9 @@ public class InBuildingData : MonoBehaviour
                 for (int y = 0; y < _whiteAreaSize.y; y++)
                 {
                     Vector3Int pos = new Vector3Int(cellPos.x + x, cellPos.y + y, 0);
+                    TileBase currentTile = GridBuildingSystem.Instance.MainTilemap.GetTile(pos);
+                    if (currentTile == whiteTile) continue; // 이미 휜 타일이 있다면 무시
+                    
                     GridBuildingSystem.Instance.MainTilemap.SetTile(pos, whiteTile);
                 }
             }
